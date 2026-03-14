cc3510a15e5a5408aea7f4da648adb9fae50304e
Sparsh Singh <sparshsinghjaduvanshi@gmail.com>
2026-03-14 18:43:59 +0530

    util code discussion

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credentials: true
+    }
+)) //to config. cors
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public")) // if we want to store some assets to my server i can use this to save in folder public
+app.use(cookieParser)
+
+// export default app // OR
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 // require('dotenv').config({path: './env'})  //one way
 import dotenv from "dotenv"; //2nd way but now we need to config it
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
     path: './env'
 }) // but now we cannot use it directly we need to add something in our .json file under /script/dev
 
 /* 2nd APPROACH */
 connectDB()
+.then( () =>{
+
+    app.on("error", (error) =>{           //it is a listening event for error is any 
+            console.log("error", error)
+            throw error
+    })
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err) =>{
+    console.log("MONGO db connection failed !!! ", err)
+})
 
 
 /* FIRST APPROACH
